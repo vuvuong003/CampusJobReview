777fdafd99e0d844357a77f3365d2641fdcf7a27
Tahreem Yasir <tahreemyasir@Tahreems-MacBook-Pro.local>
2024-10-27 14:20:16 -0400

    Updating views.py

--- review_backend/service/views.py
+++ review_backend/service/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Reviews, Vacancies
+from .serializers import ReviewsSerializer, VacanciesSerializer
 # Create your views here.
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing review instances.
+    """
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+
+
+class VacanciesViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing vacancy instances.
+    """
+    queryset = Vacancies.objects.all()
+    serializer_class = VacanciesSerializer
