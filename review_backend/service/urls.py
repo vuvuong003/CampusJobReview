30966203b4ea40aa9bb34562e50e28d971a4dd88
Tahreem Yasir <tahreemyasir@Tahreems-MacBook-Pro.local>
2024-10-28 15:59:04 -0400

    Update views.py urls.py and views.py for filtering reviews based on the search queries

--- review_backend/service/urls.py
+++ review_backend/service/urls.py
@@ -4,12 +4,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import ReviewsViewSet
 from .views import VacanciesViewSet
+from .views import FilterReviewsView
+
 router = DefaultRouter()
 router.register(r'reviews', ReviewsViewSet)
 router.register(r'vacancies', VacanciesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('reviews/filter/', FilterReviewsView.as_view(), name='filter-reviews'),
 ]
 
 
--- review_backend/service/views.py
+++ review_backend/service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Reviews
@@ -6,6 +7,7 @@ from .serializers import ReviewsSerializer
 
 from .models import Vacancies
 from .serializers import VacanciesSerializer
+
 
 class ReviewsViewSet(viewsets.ModelViewSet):
     queryset = Reviews.objects.all()  # Get all reviews
@@ -17,6 +19,30 @@ class ReviewsViewSet(viewsets.ModelViewSet):
             serializer.save()  # Save the valid data to the database
             return Response(serializer.data, status=status.HTTP_201_CREATED)  # Return created response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Return error response
+    
+class FilterReviewsView(generics.ListAPIView):
+    serializer_class = ReviewsSerializer
+
+    def get_queryset(self):
+        queryset = Reviews.objects.all()
+        department = self.request.query_params.get('department', None)
+        locations = self.request.query_params.get('locations', None)
+        job_title = self.request.query_params.get('job_title', None)
+        min_rating = self.request.query_params.get('min_rating', None)
+        max_rating = self.request.query_params.get('max_rating', None)
+
+        if department:
+            queryset = queryset.filter(department__icontains=department)
+        if locations:
+            queryset = queryset.filter(locations__icontains=locations)
+        if job_title:
+            queryset = queryset.filter(job_title__icontains=job_title)
+        if min_rating:
+            queryset = queryset.filter(rating__gte=min_rating)
+        if max_rating:
+            queryset = queryset.filter(rating__lte=max_rating)
+
+        return queryset    
 
 
 class VacanciesViewSet(viewsets.ModelViewSet):
